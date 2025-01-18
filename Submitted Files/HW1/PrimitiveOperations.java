47b1d9d472da72bcb26f86bdd459c66677198a19
alexz <alexzhang624@gmail.com>
2025-01-17 23:13:47 -0500

    Added HW1

---
+++ Submitted Files/HW1/PrimitiveOperations.java
@@ -0,0 +1,24 @@
+//I worked on the assignment alone, using only course-provided materials.
+public class PrimitiveOperations {
+    public static void main(String[] args) {
+        int number1 = 5;
+        double number2 = 3.5;
+        System.out.println(number1);
+        System.out.println(number2);
+
+        double product = number1 * number2;
+        System.out.println(product);
+
+        double convertNumber1 = (double) number1;
+        System.out.println(convertNumber1);
+
+        int convertNumber2 = (int)number2;
+        System.out.println(convertNumber2);
+
+        char letter = 'A';
+        System.out.println(letter);
+
+        letter = (char)((int)'A' + 32);
+        System.out.println(letter);
+    }
+}
---
+++ src/HW1/StringOperations.java
@@ -0,0 +1,19 @@
+package HW1;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        String name = "Hanze Zhang";
+        System.out.println(name);
+
+        name = "A" + name.substring(1,11);
+        name = name.substring(0,10) + "Z";
+        System.out.println(name);
+
+        String address = "www.gatech.edu";
+        System.out.println(address);
+
+        address = address.substring(address.indexOf('.')+1);
+        address = address.substring(0,address.indexOf('.')) + "1331";
+        System.out.println(address);
+    }
+}
